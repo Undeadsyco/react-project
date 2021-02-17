b9f7f071e53d1c782d304877558f17f1ffa1a2d0
brandon <undeadsyco112358@outlook.com>
2021-02-17 08:29:02 -0600

    completed UI/ will update later

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App=()=>{
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -46,8 +47,12 @@ const App=()=>{
 
   return (
     <div className="container">
-      <Header title='Task Tracker'/>
-      <AddTask onAdd={addTask}/>
+      <Header 
+        onAdd={()=> setShowAddTask(!showAddTask)} 
+        title='Task Tracker'
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks 
         tasks={tasks} 
         onDelete={deleteTask}
